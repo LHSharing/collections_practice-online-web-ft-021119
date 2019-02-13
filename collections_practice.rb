3c7b97635ed5d2536dc321360cedb8c66ad26315
Lu Daley <daleyluis@gmail.com>
2019-02-13 18:35:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -82,13 +82,10 @@ def sum_array(array)
 #array = [11, 4, 7, 8, 9, 100, 134]
 sum = 0
 array.each do |i|
+#  binding.pry
 sum += i
   end
   sum
-
+#binding.pry
   # i = 0
-#     while i < array.length
-#       array_total += array[i]
-#       i += 1
-#     end
 end
